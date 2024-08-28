ce5048e684dfdb5c74f423bccd6a3ee775d6d1af
Kyle Bradshaw <itllbok82@gmail.com>
2024-08-28 18:16:54 -0400

    learnign the thigns

--- dl.py
+++ dl.py
@@ -1,8 +1,16 @@
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from tensorflow import keras
+
+layers = tf.keras.layers
 
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
 
 print(train_images.ndim)
 
 print(train_images.shape)
+
+model = keras.Sequential([
+  layers.Dense(512, activation='relu'),
+  layers.Dense(10, activation='softmax')
+])
