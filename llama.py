ce15cad40bbe43b0e572a5013bb8c21799363890
Kyle Bradshaw <itllbok82@gmail.com>
2024-08-29 01:12:44 -0400

    looking at a walk through

--- llama.py
+++ llama.py
@@ -1,6 +1,27 @@
 from datasets import load_dataset
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments
 
 dataset = load_dataset("Where can I get a dataset?")
 
+# I don't actually know if this is a valid model or the correct way to load it
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B")
+
+def toenizer_function(examples):
+  return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+tokenized_datasets = dataset.map(toenizer_function, batched=True)
+
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B")
+
+training_args = TrainingArguments(
+  output_dir="./results",
+  per_device_train_batch_size=2,
+  per_device_eval_batch_size=2,
+  num_train_epochs=3,
+  logging_dir="./logs",
+  logging_steps=10,
+  save_steps=500,
+  evaluation_strategy="steps",
+  eval_steps=500,
+)
+
