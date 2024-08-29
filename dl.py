f68877cb4599ff318487486fc3ca855898b8a3fc
Kyle Bradshaw <itllbok82@gmail.com>
2024-08-29 00:52:32 -0400

    working on the things

--- dl.py
+++ dl.py
@@ -2,9 +2,14 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 
-layers = tf.keras.layers
+layers = keras.layers
 
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype("float32") / 255
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype("float32") / 255
 
 print(train_images.ndim)
 
@@ -14,3 +19,13 @@ model = keras.Sequential([
   layers.Dense(512, activation='relu'),
   layers.Dense(10, activation='softmax')
 ])
+
+model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=5, batch_size=128)
+
+test_digits = test_images[0:10]
+predictions = model.predict(test_digits)
+print(predictions[0])
+
+# Why are they using the pyshell?
--- love.py
+++ love.py
@@ -2,6 +2,7 @@ import torch
 import transformers
 import numpy as np
 import re
+from datasets import load_dataset
 
 model_id = "meta-llama/Meta-Llama-3-8B"
 pipeline = transformers.pipeline("text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto")
