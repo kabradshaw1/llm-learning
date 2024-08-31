d2110f053b3691cb88aa8ecdec2f97a4cfb6ad68
Kyle Bradshaw <itllbok82@gmail.com>
2024-08-31 15:53:16 -0400

    learnign the python a dl things

--- dl1.py
+++ dl1.py
@@ -1,22 +1,18 @@
 import numpy as np
 
 # example of scalars (rank-0 tensors)
-x = np.array(12)
-print(x)
-print(x.ndim)
+a = np.array(12)
 
 #example of a vector (rank-1 tensor)
-x = np.array([12, 3, 6, 14, 7])
-print(x)
-print(x.ndim)
+b = np.array([12, 3, 6, 14, 7])
 
 # example of a matrix (rank-2 tensor)
-x = np.array([[5, 78, 2, 34, 0],
+c = np.array([[5, 78, 2, 34, 0],
  [6, 79, 3, 35, 1],
  [7, 80, 4, 36, 2]])
 
 # (rank-3 tensor)
-x = np.array([[[5, 78, 2, 34, 0],
+d = np.array([[[5, 78, 2, 34, 0],
  [6, 79, 3, 35, 1],
  [7, 80, 4, 36, 2]],
  [[5, 78, 2, 34, 0],
@@ -25,3 +21,6 @@ x = np.array([[[5, 78, 2, 34, 0],
  [[5, 78, 2, 34, 0],
  [6, 79, 3, 35, 1],
  [7, 80, 4, 36, 2]]])
+
+# this will give a tuple that discribes the shape of the tensor
+a.shape
