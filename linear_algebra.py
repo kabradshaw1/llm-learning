1dde42df17e58d5c116d2934d162e91a869c768a
Kyle Bradshaw <itllbok82@gmail.com>
2024-08-31 19:10:46 -0400

    remembering how long ago algebra was

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 def naive_relu(x):
   assert len(x.shape) == 2
@@ -8,6 +9,21 @@ def naive_relu(x):
       x[i, j] = max(x[i, j], 0)
   return x
 
-x = np.array([[1, -1], [-1, 1]])
+def naive_add(x, y):
+  assert len(x.shape) == 2
+  assert x.shape == y.shape
+  x = x.copy()
+  for i in range(x.shape[0]):
+    for j in range(x.shape[1]):
+      x[i, j] += y[i, j]
+  return x
 
-print(naive_relu(x))
+x = np.array([[1, 10], [3, 6]])
+
+y = np.array([[2, 3], [4, 5]])
+
+z = x + y
+z = np.maximum(z, 0.)
+
+t0 = time.time()
+
