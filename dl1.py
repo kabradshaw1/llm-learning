239357567f4b679b8df9fb93b99b6475a0dbde0b
Kyle Bradshaw <itllbok82@gmail.com>
2024-08-31 14:41:41 -0400

    earning the things

---
+++ dl1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# example of scalars (rank-0 tensors)
+x = np.array(12)
+print(x)
+print(x.ndim)
+
+#example of a vector (rank-1 tensor)
+x = np.array([12, 3, 6, 14, 7])
+print(x)
+print(x.ndim)
