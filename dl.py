171d0f8ee9e93dbe8ad759bbc69d842d31d7cb15
Kyle Bradshaw <itllbok82@gmail.com>
2024-08-31 02:53:02 -0400

    blah

--- dl.py
+++ dl.py
@@ -2,11 +2,17 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 
-
 layers = keras.layers
 
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+print(train_labels)
 
+model = keras.Sequential([
+  layers.Dense(512, activation='relu'),
+  layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 # train_images = train_images.reshape((60000, 28 * 28))
 # train_images = train_images.astype("float32") / 255
 # test_images = test_images.reshape((10000, 28 * 28))
@@ -16,12 +22,9 @@ layers = keras.layers
 
 # print(train_images.shape)
 
-# model = keras.Sequential([
-#   layers.Dense(512, activation='relu'),
-#   layers.Dense(10, activation='softmax')
-# ])
 
-# model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+
 
 # model.fit(train_images, train_labels, epochs=5, batch_size=128)
 
