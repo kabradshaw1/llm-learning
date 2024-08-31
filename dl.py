62abb3ac0f3a51d3cd6857d39ddf24f1408c226c
Kyle Bradshaw <itllbok82@gmail.com>
2024-08-31 03:03:13 -0400

    working on the things

--- dl.py
+++ dl.py
@@ -1,11 +1,14 @@
-import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 
 layers = keras.layers
 
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
-print(train_labels)
+
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype("float32") / 255
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype("float32") / 255
 
 model = keras.Sequential([
   layers.Dense(512, activation='relu'),
@@ -13,23 +16,5 @@ model = keras.Sequential([
 ])
 
 model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-# train_images = train_images.reshape((60000, 28 * 28))
-# train_images = train_images.astype("float32") / 255
-# test_images = test_images.reshape((10000, 28 * 28))
-# test_images = test_images.astype("float32") / 255
 
-# print(train_images.ndim)
-
-# print(train_images.shape)
-
-
-
-
-
-# model.fit(train_images, train_labels, epochs=5, batch_size=128)
-
-# test_digits = test_images[0:10]
-# predictions = model.predict(test_digits)
-# print(predictions[0])
-
-# # Why are they using the pyshell?
+model.fit(train_images, train_labels, epochs=5, batch_size=128)
