87059eb45c57291150e6d75901a63c855c8eb5dc
Kyle Bradshaw <itllbok82@gmail.com>
2024-09-01 01:16:59 -0400

    working on the things

--- tensorflow_replacement.py
+++ tensorflow_replacement.py
@@ -4,8 +4,17 @@ class NaiveDense:
   def __init__(self, input_size, output_size, activation):
     self.activation = activation
 
-    self.weights = tf.Variable(tf.random.uniform((input_size, output_size)))
-    self.biases = tf.Variable(tf.zeros(output_size))
+    w_shape = (input_size, output_size)
+    w_initial_value = tf.random.uniform(w_shape, minval=0, maxval=1e-1)
+    self.W = tf.Variable(w_initial_value)
+
+    b_shape = (output_size,)
+    b_initial_value = tf.zeros(b_shape)
+    self.b = tf.Variable(b_initial_value)
 
   def __call__(self, inputs):
-    return self.activation(tf.matmul(inputs, self.weights) + self.biases)
+    return self.activation(tf.matmul(inputs, self.W) + self.b)
+  
+  @property
+  def weights(self):
+    return [self.W, self.b]
