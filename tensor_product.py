10f3f104e00990cbf3e37a491bac3d201ccde28a
Kyle Bradshaw <itllbok82@gmail.com>
2024-08-31 20:38:46 -0400

    linear algibra

--- tensor_product.py
+++ tensor_product.py
@@ -1,25 +1,29 @@
 import numpy as np
 
-x = np.random.random((32, 32))
-y = np.random.random((32,))
-z = np.dot(x, y)
+x = np.random.random((3, 2))
+y = np.random.random((2, 3))
+print(x)
+print(y)
+print(x.shape[1])
+print(y.shape[0])
+# z = np.dot(x, y)
 
-def naive_vector_dot(x, y):
-  assert len(x.shape) == 1
-  assert len(y.shape) == 1
-  assert x.shape[0] == y.shape[0]
-  z = 0.
-  for i in range(x.shape[0]):
-    z += x[i] * y[i]
-  return z
+# def naive_vector_dot(x, y):
+#   assert len(x.shape) == 1
+#   assert len(y.shape) == 1
+#   assert x.shape[0] == y.shape[0]
+#   z = 0.
+#   for i in range(x.shape[0]):
+#     z += x[i] * y[i]
+#   return z
 
-print(naive_vector_dot(x[0], y))
-print(z)
+# print(naive_vector_dot(x[0], y))
+# print(z)
 
-def naive_matrix_vector_dot(x, y):
-  z = np.zeros(x.shape[0])
-  for i in range(x.shape[0]):
-    z[i] = naive_vector_dot(x[i, :], y)
-  return z
+# def naive_matrix_vector_dot(x, y):
+#   z = np.zeros(x.shape[0])
+#   for i in range(x.shape[0]):
+#     z[i] = naive_vector_dot(x[i, :], y)
+#   return z
 
-print(naive_matrix_vector_dot(x, y))
+# print(naive_matrix_vector_dot(x, y))
