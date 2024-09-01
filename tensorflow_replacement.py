78e3e1197403843bc1b721f6882b80b90c9edf21
Kyle Bradshaw <itllbok82@gmail.com>
2024-09-01 01:42:47 -0400

    I dont really know what to think

--- tensorflow_replacement.py
+++ tensorflow_replacement.py
@@ -1,5 +1,11 @@
 import tensorflow as tf
-import math
+mnist = tf.keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype("float32") / 255
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype("float32") / 255
 
 class NaiveDense:
   def __init__(self, input_size, output_size, activation):
@@ -56,3 +62,30 @@ class BatchGenerator:
     labels = self.labels[self.index:self.index+self.batch_size]
     self.index += self.batch_size
     return images, labels
+  
+def one_training_step(model, images_batch, labels_batch):
+  with tf.GradientTape() as tape:
+    predictions = model(images_batch)
+    per_sample_losses = tf.keras.losses.sparse_categorical_crossentropy(labels_batch, predictions)
+    average_loss = tf.reduce_mean(per_sample_losses)
+  gradients = tape.gradient(average_loss, model.weights)
+  update_weights(gradients, model.weights)
+  return average_loss
+
+learning_rate = 1e-3
+
+def update_weights(gradients, weights):
+  for g, w in zip(gradients, weights):
+    w.assign_sub(g * learning_rate)
+
+def fit(model, images, labels, epochs, batch_size=128):
+  for epoch_counter in range(epochs):
+    print(f"Epoch {epoch_counter}")
+    batch_generator = BatchGenerator(images, labels)
+    for batch_counter in range(len(images)//batch_size):
+      images_batch, labels_batch = batch_generator.next()
+      loss = one_training_step(model, images_batch, labels_batch)
+      if batch_counter % 100 == 0:
+        print(f"loss at batch {batch_counter}: {loss:.2f}")
+
+fit(model, train_images, train_labels, epochs=10, batch_size=128)
