cfbb6e9d0807b0ec43baadad88da1c4d2d48523e
Kyle Bradshaw <itllbok82@gmail.com>
2024-08-31 20:10:09 -0400

    working on the things

--- broadcast.py
+++ broadcast.py
@@ -3,11 +3,23 @@ import numpy as np
 x = np.random.random((32, 10))
 print('x:', x)
 y = np.random.random((10,))
-print('before expand', y.shape)
-print('y:', y)
-y = np.expand_dims(y, axis=0)
-print('after expand', y.shape)
-print('y after expand:', y)
+# print('before expand', y.shape)
+# print('y:', y)
+# y = np.expand_dims(y, axis=0)
+# print('after expand', y.shape)
+# print('y after expand:', y)
 
-y = np.concatenate([y] * 32, axis=0)
-print('y after concatenate:', y)
+# y = np.concatenate([y] * 32, axis=0)
+# print('y after concatenate:', y)
+
+def naive_add_matrix_and_vector(x, y):
+  assert len(x.shape) == 2
+  assert len(y.shape) == 1
+  assert x.shape[1] == y.shape[0]
+  x = x.copy()
+  for i in range(x.shape[0]):
+    for j in range(x.shape[1]):
+      x[i, j] += y[j]
+  return x
+
+print(naive_add_matrix_and_vector(x, y))
