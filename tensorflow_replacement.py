ee649981f2de53749dadbe5aea46b3ffb375b5c4
Kyle Bradshaw <itllbok82@gmail.com>
2024-09-01 01:22:40 -0400

    blah

--- tensorflow_replacement.py
+++ tensorflow_replacement.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import math
 
 class NaiveDense:
   def __init__(self, input_size, output_size, activation):
@@ -18,3 +19,40 @@ class NaiveDense:
   @property
   def weights(self):
     return [self.W, self.b]
+  
+class NaiveSequential:
+  def __init__(self, layers):
+    self.layers = layers
+  
+  def __call__(self, inputs):
+    x = inputs
+    for layer in self.layers:
+      x = layer(x)
+    return x
+  
+  @property
+  def weights(self):
+    weights = []
+    for layer in self.layers:
+      weights += layer.weights
+    return weights
+  
+model = NaiveSequential([
+  NaiveDense(input_size=28*28, output_size=512, activation=tf.nn.relu),
+  NaiveDense(input_size=512, output_size=10, activation=tf.nn.softmax)
+])
+
+assert len(model.weights) == 4
+
+class BatchGenerator:
+  def __init__(self, images, labels, batch_size=128):
+    self.index = 0
+    self.images = images
+    self.labels = labels
+    self.batch_size = batch_size
+  
+  def next(self):
+    images = self.images[self.index:self.index+self.batch_size]
+    labels = self.labels[self.index:self.index+self.batch_size]
+    self.index += self.batch_size
+    return images, labels
