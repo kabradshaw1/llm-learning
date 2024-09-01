6fafd5169c47ed61eeba50b4921e73a33b559f5b
Kyle Bradshaw <itllbok82@gmail.com>
2024-08-31 20:24:20 -0400

    page 42

---
+++ tensor_product.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+x = np.random.random((32, 32))
+y = np.random.random((32,))
+z = np.dot(x, y)
+
+def naive_vector_dot(x, y):
+  assert len(x.shape) == 1
+  assert len(y.shape) == 1
+  assert x.shape[0] == y.shape[0]
+  z = 0.
+  for i in range(x.shape[0]):
+    z += x[i] * y[i]
+  return z
+
+print(naive_vector_dot(x[0], y))
+print(z)
+
+def naive_matrix_vector_dot(x, y):
+  z = np.zeros(x.shape[0])
+  for i in range(x.shape[0]):
+    z[i] = naive_vector_dot(x[i, :], y)
+  return z
+
+print(naive_matrix_vector_dot(x, y))
